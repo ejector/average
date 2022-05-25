cd79fc525bcb26e4b44ae23975f6b25f6dd06c9e
deboloed <deboloed@gmail.com>
2022-05-26 00:04:01 +0300

    added client class

---
+++ client/src/client/client.cpp
@@ -0,0 +1,32 @@
+#include "client.h"
+
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+
+namespace average {
+
+Client::Client(io_context &context)
+    : _context(context)
+    , _socket(context)
+{
+}
+
+void Client::connect(const std::string &ip, int port)
+{
+    _socket.connect(ip::tcp::endpoint(ip::address::from_string(ip), port));
+}
+
+void Client::write(int data)
+{
+    boost::asio::write(_socket, boost::asio::buffer(&data, sizeof(data)));
+}
+
+int Client::read()
+{
+    int data;
+    boost::asio::read(_socket, boost::asio::buffer(&data, sizeof(data)));
+    return data;
+}
+
+}
---
+++ client/src/client/client.h
@@ -0,0 +1,28 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <string>
+
+#include <boost/asio.hpp>
+
+namespace average {
+
+class Client
+{
+public:
+    using io_context = boost::asio::io_context;
+    using tcp_socket = boost::asio::ip::tcp::socket;
+
+    Client(io_context &context);
+    void connect(const std::string &ip, int port);
+    void write(int random);
+    int read();
+
+protected:
+    io_context &_context;
+    tcp_socket _socket;
+};
+
+} // average
+
+#endif // CLIENT_H
